bf9b4639725f8866ee4d75b5a79717b12ae7b6f8
Thomas Bennett <sc20tmb@leeds.ac.uk>
2023-11-15 22:07:36 +0000

    Set up database writing and allowed for variablity in generation numbers and realism

--- src/main/java/com/function/Task1.java
+++ src/main/java/com/function/Task1.java
@@ -8,15 +8,18 @@ import com.microsoft.azure.functions.HttpStatus;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
+
 import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.concurrent.TimeUnit;
+
+import java.sql.*;
 
 /**
  * Azure Functions with HTTP Trigger.
@@ -24,12 +27,12 @@ import java.util.Random;
 public class Task1 {
     public static class SensorData
     {
-        int id;
-        int temperature;
-        int wind;
-        int rHumidity;
-        int co2;
-        LocalDateTime timestamp;
+        private int id;
+        private int temperature;
+        private int wind;
+        private int rHumidity;
+        private int co2;
+        private LocalDateTime timestamp;
 
         public SensorData(int sensorId, int sensorTemperature, int sensorWind, int sensorRHumidity, int sensorCo2)
         {
@@ -40,6 +43,31 @@ public class Task1 {
             this.co2 = sensorCo2;
             this.timestamp = LocalDateTime.now();
         }
+
+        public int getId()
+        {
+            return this.id;
+        }
+        public int getTemperature()
+        {
+            return this.temperature;
+        }
+        public int getWind()
+        {
+            return this.wind;
+        }
+        public int getRHumidity()
+        {
+            return this.rHumidity;
+        }
+        public int getCO2()
+        {
+            return this.co2;
+        }
+        public LocalDateTime getTimestamp()
+        {
+            return this.timestamp;
+        }
     }
 
     protected static int GenerateRandomValue(Random seed, int start, int end)
@@ -47,10 +75,26 @@ public class Task1 {
         return seed.nextInt(start, end+1);
     }
 
+    protected static boolean CreateTable(Connection connection, ExecutionContext context)
+    {
+        try
+        {
+            // Drop the table if it already exists
+            PreparedStatement query = connection.prepareStatement("IF OBJECT_ID('sensorData', 'U') IS NOT NULL DROP TABLE sensorData;" + //
+                                                                  "CREATE TABLE sensorData (id int NOT NULL, temperature int NOT NULL, wind int NOT NULL, rHumidity int NOT NULL, co2 int NOT NULL, timestamp datetime NOT NULL)");
+            query.executeUpdate();
+            return true;
+        }
+        catch (SQLException e)
+        {
+            context.getLogger().log(Level.SEVERE, e.getMessage());
+            return false;
+        }
+    }
+
     /**
-     * This function listens at endpoint "/api/HttpExample". Two ways to invoke it using "curl" command in bash:
-     * 1. curl -d "HTTP Body" {your host}/api/HttpExample
-     * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
+     * This function listens at endpoint "/api/Task1". Two ways to invoke it using "curl" command in bash:
+     * 1. curl -X POST --data "HTTP Body" {your host}/api/Task1
      */
     @FunctionName("Task1")
     public HttpResponseMessage run(
@@ -67,23 +111,34 @@ public class Task1 {
         {
             return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a number of sensors in the request body").build();
         }
+
         final String body = request.getBody().get();
 
         ObjectMapper mapper = new ObjectMapper();
-        final String numSensorsString;
+        final String numIterationsString;
+        final String simulateDelayedReadingsString;
 
         // Check the numSensors parameter exists
         try
         {
             JsonNode node = mapper.readTree(body);
-            if (node.has("numSensors"))
+            if (node.has("numIterations"))
             {
-                numSensorsString = node.get("numSensors").asText();
+                numIterationsString = node.get("numIterations").asText();
             }
             else
             {
                 return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a number of sensors in the request body").build();
-            }    
+            }
+
+            if (node.has("simulateDelayedReadings"))
+            {
+                simulateDelayedReadingsString = node.get("simulateDelayedReadings").asText();
+            }
+            else
+            {
+                return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass whether you would like to simulate realistic readings in thte request body").build();
+            }
         }
         catch (Exception e)
         {
@@ -91,29 +146,91 @@ public class Task1 {
         }
 
 
-        // Check the numSensors parameter is a number
-        int numSensors = 0;
+        // Check the numIterations parameter is a number
+        int numIterations = 0;
         try
         {
-            numSensors = Integer.parseInt(numSensorsString);
+            numIterations = Integer.parseInt(numIterationsString);
         }
         catch (NumberFormatException e)
         {
             return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a number of sensors in the request body").build();
         }
 
-        ArrayList<SensorData> sensorData = new ArrayList<SensorData>();
+        // Get the value of simulateDelayedReadings
+        boolean simulateDelayedReadings = false;
+
+        simulateDelayedReadings = Boolean.parseBoolean(simulateDelayedReadingsString);
+
         Random seed = new Random();
 
-        for (int id = 1; id <= numSensors; id++)
-        {
-            sensorData.add(new SensorData(id, GenerateRandomValue(seed, 8, 15),
-                                        GenerateRandomValue(seed, 15, 25),
-                                        GenerateRandomValue(seed, 40, 70),
-                                        GenerateRandomValue(seed, 500, 1500)));
-        }
+        // Write the sensor data to the db
+        String connectionString = System.getenv("DB_CONNECTION_STRING");
+        Connection connection;
+        PreparedStatement query;
 
-        return request.createResponseBuilder(HttpStatus.OK).body("Hello").build();
+        try
+        {
+            // Get connection to DB and create the table
+            connection = DriverManager.getConnection(connectionString);
+
+            if (!CreateTable(connection, context))
+            {
+                return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred while creating table.").build();
+            }
+
+            // For the number of specified iterations
+            query = connection.prepareStatement("INSERT INTO sensorData VALUES (?, ?, ?, ?, ?, ?)");
+            for (int iteration = 0; iteration < numIterations; iteration++)
+            {
+                // For each set of readings, generate 20 random readings
+                // Insert each reading as a new row in the database
+                for (int id = 1; id <= 20; id++)
+                {
+                    SensorData currentSensor = new SensorData(id, GenerateRandomValue(seed, 8, 15),
+                                                GenerateRandomValue(seed, 15, 25),
+                                                GenerateRandomValue(seed, 40, 70),
+                                                GenerateRandomValue(seed, 500, 1500));
+
+                    query.setString(1, String.valueOf(currentSensor.getId()));
+                    query.setString(2, String.valueOf(currentSensor.getTemperature()));
+                    query.setString(3, String.valueOf(currentSensor.getWind()));
+                    query.setString(4, String.valueOf(currentSensor.getRHumidity()));
+                    query.setString(5, String.valueOf(currentSensor.getCO2()));
+                    query.setTimestamp(6, Timestamp.valueOf(currentSensor.getTimestamp()));
+                    
+                    query.executeUpdate();
+                }
+                // Pause for 2 seconds to simulate staggered readings
+                if (simulateDelayedReadings)
+                {
+                    try
+                    {
+                        TimeUnit.SECONDS.sleep(2);
+                    }
+                    catch (InterruptedException e)
+                    {
+                        context.getLogger().log(Level.SEVERE, e.getMessage());
+                        return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR).body("Encountered an unexpected error when simulating delayed readings.").build();
+                    }
+                    
+                }
+            }  
+            connection.close();
+            query.close();
+        }
+        catch (SQLException e)
+        {
+            context.getLogger().log(Level.SEVERE, e.getMessage());
+            return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR).body("Encountered an SQL error during the database connection.").build();
+        }
+        /*catch (InterruptedException e)
+        {
+            context.getLogger().log(Level.SEVERE, e.getMessage());
+            return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR).body("Encountered an unexpected error.").build();
+        }*/
+
+        return request.createResponseBuilder(HttpStatus.OK).body("Successfully Generated and Stored Sensor Data :)").build();
 
     }
 }
