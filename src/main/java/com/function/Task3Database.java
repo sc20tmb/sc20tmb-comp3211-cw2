83a9ed16dc1ca68ff885a800fd32f6933c520d27
Thomas Bennett <sc20tmb@leeds.ac.uk>
2023-11-18 19:25:50 +0000

    Generating data and writing to database

---
+++ src/main/java/com/function/Task3Database.java
@@ -0,0 +1,79 @@
+package com.function;
+
+import java.sql.*;
+import java.util.*;
+import java.util.logging.Level;
+
+import com.microsoft.azure.functions.annotation.*;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.microsoft.azure.functions.*;
+
+/**
+ * Azure Functions with HTTP Trigger.
+ */
+public class Task3Database {
+    /**
+     * This function listens at endpoint "/api/Task3Database". Two ways to invoke it using "curl" command in bash:
+     * 1. curl -d "HTTP Body" {your host}/api/Task3Database
+     */
+    @FunctionName("Task3Database")
+    public HttpResponseMessage run(
+            @HttpTrigger(name = "req", methods = {HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context) {
+        context.getLogger().info("Java HTTP trigger processed a request.");
+
+        final String body = request.getBody().get();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        ArrayList<SensorData> sensorData;
+
+        // Desrialise the request body
+        try
+        {
+            sensorData = mapper.readValue(body, new TypeReference<ArrayList<SensorData>>() {});
+        }
+        catch (Exception e)
+        {
+            context.getLogger().log(Level.SEVERE, e.getMessage());
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).build();
+        }
+
+        // Write values to the Database
+        Connection connection;
+        PreparedStatement query;
+        final String connectionString = System.getenv("DB_CONNECTION_STRING");
+
+        try
+        {
+            connection = DriverManager.getConnection(connectionString);
+
+            // Assume table already created
+            query = connection.prepareStatement("INSERT INTO sensorData VALUES (?, ?, ?, ?, ?, ?)");
+
+            for (SensorData sensor : sensorData) {
+                query.setString(1, String.valueOf(sensor.getId()));
+                query.setString(2, String.valueOf(sensor.getTemperature()));
+                query.setString(3, String.valueOf(sensor.getWind()));
+                query.setString(4, String.valueOf(sensor.getRHumidity()));
+                query.setString(5, String.valueOf(sensor.getCO2()));
+                query.setTimestamp(6, Timestamp.valueOf(sensor.getTimestamp()));
+
+                query.addBatch();
+            }
+
+            query.executeBatch();
+
+            connection.close();
+            query.close();
+        }
+        catch (SQLException e)
+        {
+            context.getLogger().log(Level.SEVERE, e.getMessage());
+            return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
+        return request.createResponseBuilder(HttpStatus.OK).build();
+    }
+}
---
+++ src/main/java/com/function/Task3Generation.java
@@ -0,0 +1,107 @@
+package com.function;
+
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.logging.Level;
+
+import com.microsoft.azure.functions.annotation.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.microsoft.azure.functions.*;
+
+/**
+ * Azure Functions with Timer trigger.
+ */
+public class Task3Generation {
+    protected static int GenerateRandomValue(Random seed, int start, int end)
+    {
+        return seed.nextInt(start, end+1);
+    }
+
+    /**
+     * This function will be invoked every 5 seconds, generating the sensor data.
+     */
+    @FunctionName("Task3Generation")
+    public void run(
+        @TimerTrigger(name = "timerInfo", schedule = "*/5 * * * * *") String timerInfo,
+        final ExecutionContext context
+    ) {
+        
+        // Initialising variables
+        ArrayList<SensorData> data = new ArrayList<>();
+        Random seed = new Random();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        String body;
+
+        // Generate the data for 20 sensors
+        for (int id = 1; id <= 20; id++)
+        {
+            SensorData currentSensor = new SensorData(id, GenerateRandomValue(seed, 8, 15),
+                                        GenerateRandomValue(seed, 15, 25),
+                                        GenerateRandomValue(seed, 40, 70),
+                                        GenerateRandomValue(seed, 500, 1500));
+
+            data.add(currentSensor);
+        }
+
+        // Serialise the data
+        try
+        {
+            body = mapper.writeValueAsString(data);
+        }
+        catch (JsonProcessingException e)
+        {
+            context.getLogger().log(Level.SEVERE, e.getMessage());
+            return; // Do not attempt to send data
+        }
+
+        // Send data to Database function
+        try
+        {
+            URL url = new URL("https://comp3211-sc20tmb-cw1.azurewebsites.net/api/Task3Database");
+            
+            HttpURLConnection connection = ((HttpURLConnection)url.openConnection());
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setDoOutput(true);
+
+            OutputStream out = connection.getOutputStream();
+            out.write(body.getBytes());
+
+            switch(connection.getResponseCode())
+            {
+                case 200: // OK
+                {
+                    context.getLogger().log(Level.INFO, "Successfully generated and stored data.");
+                } break;
+
+                case 400: // BAD_REQUEST
+                {
+                    context.getLogger().log(Level.INFO, "There was a problem with the request.");
+                } break;
+
+                case 500: // INTERNAL_SERVER_ERROR
+                {
+                    context.getLogger().log(Level.INFO, "There was a problem connecting to the database.");
+                } break;
+
+                default:
+                {
+                    context.getLogger().log(Level.SEVERE, "Unexpected http code: " + connection.getResponseCode());
+                }
+            }
+
+            connection.disconnect();
+        }
+        catch (Exception e)
+        {
+            context.getLogger().log(Level.SEVERE, e.getMessage());
+        }
+
+    }
+}
